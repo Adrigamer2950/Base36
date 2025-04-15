81c104eef583ef0e3c19524511f9b9ec2ecdc0d1
Adrigamer2950 <adrigamer2950@gmail.com>
2025-04-15 17:10:24 +0200

    Publish artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    id("maven-publish")
 }
 
 group = "me.devadri"
@@ -21,5 +22,63 @@ tasks.test {
     useJUnitPlatform()
     testLogging {
         events("passed", "skipped", "failed")
+    }
+}
+
+tasks.register("sourcesJar", Jar::class) {
+    from(sourceSets.main.get().kotlin)
+    archiveClassifier.set("sources")
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://repo.devadri.es/repository/releases")
+            credentials {
+                username = project.findProperty("NEXUS_USERNAME") as String?
+                password = project.findProperty("NEXUS_PASSWORD") as String?
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = rootProject.group as String
+            artifactId = rootProject.name
+            version = rootProject.version as String
+
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["jar"])
+
+            pom {
+                name = rootProject.name
+                description = "Base36 implementation"
+                url = "https://github.com/Adrigamer2950/Base36"
+
+                licenses {
+                    license {
+                        name = "GPL-3.0"
+                        url = "https://www.gnu.org/licenses/gpl-3.0.html"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "devadri"
+                        name = "Adri"
+                    }
+                }
+
+                scm {
+                    url = "https://github.com/Adrigamer2950/Base36"
+                }
+
+                issueManagement {
+                    system = "GitHub"
+                    url = "https://github.com/Adrigamer2950/Base36/issues"
+                }
+            }
+        }
     }
 }
